30b9e8be62dbfdd91bbbfa5a7bde7c8348d67111
Ordin Zabek <ordin.zabek@gmail.com>
2023-04-18 19:07:52 +0800

    TailwindCss

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import './tailwind.css';
+
 import { RainbowKitProvider, connectorsForWallets, Chain } from '@rainbow-me/rainbowkit';
 import {
   argentWallet,
